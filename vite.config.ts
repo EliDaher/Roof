d28928c0da456a89dfbd467c0d6ac9372a45ec52
elidaher <eeli56315@gmail.com>
2025-08-19 14:58:36 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/Roof/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: "/Roof/",   // لازم يكون مطابق لاسم الريبو بالضبط
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
