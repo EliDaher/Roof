2750d35bb3fade8532111827ad0ebfe9e75e8631
elidaher <eeli56315@gmail.com>
2025-08-21 13:45:31 +0300

    smaller img

--- src/components/TypeBtn.tsx
+++ src/components/TypeBtn.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 type typeBtnProps = {
     icon: any,
     text: string
@@ -8,10 +6,10 @@ type typeBtnProps = {
 export default function TypeBtn({icon, text}: typeBtnProps) {
   return (
     <div className='rounded-full bg-[#09121E95] z-20 mx-5
-      flex items-center gap-5 p-3 border-2 border-white/20'
+      flex items-center gap-3 p-2 border-2 border-white/20'
     >
-      <img className='size-[50px]' src={icon} alt="" />
-      <h2 className='text-3xl font-Tinos'>{text}</h2>
+      <img className='size-[40px] ml-2 my-1' src={icon} alt="" />
+      <h2 className='text-xl font-Tinos'>{text}</h2>
     </div>
   )
 }
