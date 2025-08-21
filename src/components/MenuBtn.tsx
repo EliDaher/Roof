8fb4ef2f09746a942099f23374f736846c51138f
elidaher <eeli56315@gmail.com>
2025-08-21 13:35:35 +0300

    add to menu

--- src/components/MenuBtn.tsx
+++ src/components/MenuBtn.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function MenuBtn() {
   return (
-    <div className='flex flex-col gap-1.5 fixed top-5 right-5 justify-center items-center'>
-        <div className='h-2 w-8 bg-[#FAD29E] rounded-full'></div>
-        <div className='h-2 w-10 bg-[#FAD29E] rounded-full'></div>
-        <div className='h-2 w-8 bg-[#FAD29E] rounded-full'></div>
+    <div className='flex flex-col gap-1.5 fixed top-6 right-5 justify-center items-center'>
+        <div className='h-1.5 w-8 bg-[#FAD29E] rounded-full'></div>
+        <div className='h-1.5 w-10 bg-[#FAD29E] rounded-full'></div>
+        <div className='h-1.5 w-8 bg-[#FAD29E] rounded-full'></div>
     </div>
   )
 }
--- src/components/TypeBtn.tsx
+++ src/components/TypeBtn.tsx
@@ -7,9 +7,11 @@ type typeBtnProps = {
 
 export default function TypeBtn({icon, text}: typeBtnProps) {
   return (
-    <div className='rounded-full bg-[#09121E95] z-20 ml-7 flex items-center gap-5 p-3'>
-        <img className='size-[50px]' src={icon} alt="" />
-        <h2 className='text-3xl font-Tinos'>{text}</h2>
+    <div className='rounded-full bg-[#09121E95] z-20 mx-5
+      flex items-center gap-5 p-3 border-2 border-white/20'
+    >
+      <img className='size-[50px]' src={icon} alt="" />
+      <h2 className='text-3xl font-Tinos'>{text}</h2>
     </div>
   )
 }
